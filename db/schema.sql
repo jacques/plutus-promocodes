9d1fcbb7b28ef1ccd82ae9179edde75188ec75ba
Jacques Marneweck <jacques@powertrip.co.za>
2019-03-22 20:37:32 +0200

    More WIP

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `promocodes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `promocode` varchar(32) NOT NULL DEFAULT '',
+  `agency_id` int(11) DEFAULT NULL,
+  `agent_id` bigint(11) DEFAULT NULL,
+  `account_plan_id` int(11) DEFAULT NULL,
+  `udf1` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `promocode` (`promocode`),
+  KEY `account_plan_id` (`account_plan_id`),
+  KEY `agency_id` (`agency_id`),
+  CONSTRAINT `promocodes_ibfk_1` FOREIGN KEY (`account_plan_id`) REFERENCES `account_plans` (`id`),
+  CONSTRAINT `promocodes_ibfk_2` FOREIGN KEY (`agency_id`) REFERENCES `agencies` (`id`),
+  CONSTRIANT `promocodes_ibfk_3` FOREIGN KEY (`agent_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/routes.php
+++ src/routes.php
@@ -21,7 +21,8 @@
 
 use Plutus\Models\AccountPlan;
 use Plutus\Models\Agency;
-use Plutus\Models\PromoCode;
+use Plutus\Models\AgencyAgent;
+use Plutus\Models\Promocode;
 use Plutus\Models\User;
 
 $app->get('/admin/promocodes', $authenticate($app), $is_admin($app), function () use ($app, $config) {
@@ -29,6 +30,22 @@ $app->get('/admin/promocodes', $authenticate($app), $is_admin($app), function ()
         $app->notFound();
     }
 
+    if ($app->request()->get('agency_id')) {
+        $agency_id = $app->request()->get('agency_id');
+        $promocodes = Promocode::with('agency')
+            ->where('agency_id', $agency_id)
+            ->get();
+    } else {
+        $promocodes = Promocode::all();
+    }
+
+    $app->template->bulkAssign(
+        compact(
+            'agencies',
+            'promocodes'
+        )
+    );
+    $app->template->display('promocodes/index.tpl');
 });
 
 $app->post('/admin/promocodes', $authenticate($app), $is_admin($app), function () use ($app, $config) {
@@ -36,6 +53,41 @@ $app->post('/admin/promocodes', $authenticate($app), $is_admin($app), function (
         $app->notFound();
     }
 
+    $post = $app->request()->post();
+
+    $v = new \Valitron\Validator($post);
+    $v->rule('required', [
+        'promocode',
+        'agency_id',
+        'agent_id',
+        'account_plan_id',
+        'udf1',
+    ])->message('Please enter the {field}');
+    $v->rule('lengthMin', 'promocode', 2);
+    $v->labels([
+        'promocode'       => 'Promotional Code',
+        'account_plan_id' => 'Default Account Plan',
+        'agency_id'       => 'Assign to Agency',
+        'agent_id'        => 'Assigned to Agent',
+        'udf1'            => 'UDF1',
+    ]);
+
+    $ok = $v->validate();
+    if ($ok) {
+        $promocode = (new PromoCode());
+        $promocode->promocode = $post['promocode'];
+        $promocode->agency_id = $post['agency_id'];
+        $promocode->agent_id = $post['agent_id'];
+        $promocode->account_plan_id = $post['account_plan_id'];
+        $promocode->udf1 = $post['udf1'];
+        $promocode->save();
+
+        $app->redirect('/admin/promocodes');
+    }
+
+    var_dump($app->request()->post());
+
+    $app->template->display('promocodes/new.tpl');
 });
 
 $app->get('/admin/promocodes/new', $authenticate($app), $is_admin($app), function () use ($app, $config) {
@@ -43,6 +95,36 @@ $app->get('/admin/promocodes/new', $authenticate($app), $is_admin($app), functio
         $app->notFound();
     }
 
+    $agencies = Agency::orderBy('agency_name')
+        ->get()
+        ;
+
+    /**
+     * Agencies which have agents are shown on the dropdown.
+     */
+    $agents = AgencyAgent::selectRaw("\n            u.id AS id,\n            CONCAT(u.first_name, ' ', u.last_name) AS name,\n            agencies__agents.agency_id\n        ")
+        ->leftJoin('users as u', 'u.id', '=', 'agencies__agents.user_id')
+        ->whereNotIn('agencies__agents.agency_id', [3])
+        ->groupBy('u.id')
+        ->groupBy('agencies__agents.agency_id')
+        ->orderBy('u.first_name', 'ASC')
+        ->orderBy('u.last_name', 'ASC')
+        ->get();
+
+    $account_plans = AccountPlan::where('account_type', 'wallet')
+        ->where('active', 1)
+        ->orderBy('monthly')
+        ->get()
+        ;
+
+    $app->template->bulkAssign(
+        compact(
+            'account_plans',
+            'agencies',
+            'agents'
+        )
+    );
+    $app->template->display('promocodes/new.tpl');
 });
 
 $app->map('/admin/promocodes/:promocode/edit', $authenticate($app), $is_admin($app), function () use ($app, $config) {
